3cf05cf3b1c08fef1346235f400cc10436345dd7
lgabci <gl12qw@gmail.com>
2024-09-12 00:04:58 +0200

    mkimg.sh: set symbol value

--- src/arch/i386/emu/mkimg.sh
+++ src/arch/i386/emu/mkimg.sh
@@ -8,7 +8,7 @@ error() {
   exit 1
 }
 
-if [ $# -ne 3 ]; then
+if [ $# -ne 4 ]; then
   echo "$basename: bad argument list" >&2
   echo "\t$@" >&2
   exit 1
@@ -16,7 +16,51 @@ fi
 
 imgfile="$1"
 imgsize="$2"
-mbrbin="$3"
+mbrelf="$3"
+mbrbin="$4"
+
+# set symbol address
+# parameters: 1. img file
+#             2. elf file offset in img file
+#             3. elf file
+#             4. symbol name
+#             5. symbol lenght
+#             6. value
+set_sym() {
+  local filenam="$1"
+  local fileoff="$2"
+  local elffile="$3"
+  local symname="$4"
+  local len="$5"
+  local value="$6"
+
+  local origvalue="$value"
+  local i
+  local val
+  local vals=""
+
+  local pos=$(i386-elf-nm -t d "$elffile" | \
+              awk '{if ($3 == "beh") {print $1 + 0}}')
+  if [ -z "$pos" -o "$pos" -eq 0 ]; then
+    echo "$basename: symbol \"$symname\" not found in file $elffile" >&2
+    exit 1
+  fi
+
+  for i in $(seq 1 $len); do
+    val=$((value % 256))
+    value=$((value / 256))
+
+    vals="$vals\\$(printf "%o" $val)"
+  done
+  if [ "$value" -ne 0 ]; then
+    echo "$basename: symbol \"$symname\" value is too big for $len bytes:" \
+         "\"$origvalue\"" >&2
+    exit 1
+  fi
+
+  printf "$vals" | dd of="$filenam" bs=1 iflag=fullblock \
+                     seek=$((fileoff + pos)) conv=notrunc status=none
+}
 
 sfdisk=$(whereis -b sfdisk | awk '{print $2}')
 [ -n "$sfdisk" ] || error "no sfdisk found"
@@ -24,6 +68,8 @@ sfdisk=$(whereis -b sfdisk | awk '{print $2}')
 # create image file
 qemu-img create -q -f raw "$imgfile" "$imgsize"
 dd if="$mbrbin" of="$imgfile" bs=512 conv=notrunc status=none
+
+a=$(set_sym "$imgfile" 0 "$mbrelf" beh 1 1)
 
 # create partition table
 echo ',20M,,*' |
